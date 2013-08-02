60602d8b87fad0f4cacd17cf781a31e5e9ff058c
Andy Botting <andy@andybotting.com>
2013-08-02 15:23:36 +1000

    Make Volunteer form HTML and add vcard

--- htdocs/volunteerform.php
+++ htdocs/volunteerform.php
@@ -6,44 +6,83 @@
 
 		if ($_POST['humantest'] == '6') {
 		
-				// Get the POST fields
-				foreach(array_keys($_POST) as $k) {
-					$$k =  stripslashes($_POST[$k]);
-				}
-		
-		
-				// Formulate the email
-				$message = "Wombat's Wish Volunteer Application Form
-		
-Title:             $title
-Christian Name(s): $firstname
-Surname:           $surname
-Address:           $address
-Date of Birth:     $dateofbirth
-Telephone (H):     $telephonehome
-Telephone (W):     $telephonework
-Telephone (M):     $telephonemobile
-Email Address:     $email
-
-=> Why are you applying to be a volunteer with Wombat's Wish?
-  $whyapply
-
-=> Please tell us about your skills and experience and how you think these could be useful to this role
-  $skills
-
-=> What do you think you will gain from being a Wombat's Wish volunteer?
-  $yougain
-
-=> Please tell us about your personal experience of loss and/or bereavement, if any.
-  $personal
-
-=> Please tell us of any other relevant information- including information about your employment experience and personal interests.
-  $otherinfo";
+			// Get the POST fields
+			foreach(array_keys($_POST) as $k) {
+				$$k =  stripslashes($_POST[$k]);
+			}
 		 
+			$from = $firstname." ".$surname." <".$email.">";
 			$subject = "WW Volunteer Application from $firstname $surname";
+			#$emailaddress = "volunteer@wombatswish.org.au";
+			$emailaddress = "andy@andybotting.com";
 
-			$emailaddress = "volunteer@wombatswish.org.au";
-			mail($emailaddress, $subject, $message, "From: $firstname $surname <$email>\nX-Mailer: PHP/" . phpversion());
+			$separator = md5(time());
+			$eol = PHP_EOL;
+			$filename = "applicant.vcard";
+			$attachment = chunk_split(base64_encode($vcarddoc));
+
+			$headers  = "From: ".$from.$eol;
+			$headers .= "MIME-Version: 1.0".$eol; 
+			$headers .= "Content-Type: multipart/mixed; boundary=\"".$separator."\"";
+
+			$message = "
+<h2>Wombat's Wish Volunteer Application Form</h2>
+<h3>Personal Details</h3>
+<table>
+  <tr><td>Title:</td>         <td>$title</td></tr>
+  <tr><td>First Name:</td>    <td>$firstname</td></tr>
+  <tr><td>Surname:</td>       <td>$surname</td></tr>
+  <tr><td>Address:</td>       <td>$address</td></tr>
+  <tr><td>Date of Birth:</td> <td>$day/$month/$year</td></tr>
+  <tr><td>Telephone (H):</td> <td>$telephonehome</td></tr>
+  <tr><td>Telephone (W):</td> <td>$telephonework</td></tr>
+  <tr><td>Telephone (M):</td> <td>$telephonemobile</td></tr>
+  <tr><td>Email Address:</td> <td>$email</td></tr>
+</table>
+
+<h3>Why are you applying to be a volunteer with Wombat's Wish?</h3>
+<p>$whyapply</p>
+
+<h3>Please tell us about your skills and experience and how you think these could be useful to this role</h3>
+<p>$skills</p>
+
+<h3>What do you think you will gain from being a Wombat's Wish volunteer?</h3>
+<p>$yougain</p>
+
+<h3>Please tell us about your personal experience of loss and/or bereavement, if any.</h3>
+<p>$personal</p>
+
+<h3>Please tell us of any other relevant information- including information about your employment experience and personal interests.</h3>
+<p>$otherinfo</p>
+";
+
+			$vcard = "BEGIN:VCARD\n";
+			$vcard .= "VERSION:3.0\n";
+			$vcard .= "FN:$firstname $surname\n";
+			$vcard .= "N:".$surname.";".$firstname.";;".$title.";\n";
+			$vcard .= "BDAY:".$year."-".$month."-".$day."\n"; # 1980-12-30
+			$vcard .= "EMAIL;TYPE=INTERNET;TYPE=HOME:".$email."\n";
+			$vcard .= "TEL;TYPE=HOME:".$telephonehome."\n";
+			$vcard .= "TEL;TYPE=CELL:".$telephonemobile."\n";
+			$vcard .= "TEL;TYPE=WORK:".$telephonework."\n";
+			$vcard .= "ADR;TYPE=HOME:;;".$address.";;;;\n";
+			$vcard .= "END:VCARD\n";
+		
+			// message
+			$body  = "--".$separator.$eol;
+			$body .= "Content-Type: text/html; charset=\"iso-8859-1\"".$eol;
+			$body .= "Content-Transfer-Encoding: 7bit".$eol.$eol;
+			$body .= $message.$eol;
+
+			// attachment
+			$body .= "--".$separator.$eol;
+			$body .= "Content-Type: text/x-vcard; name=\"".$filename."\"".$eol; 
+			$body .= "Content-Transfer-Encoding: base64".$eol;
+			$body .= "Content-Disposition: attachment".$eol.$eol;
+			$body .= chunk_split(base64_encode($vcard)).$eol;
+			$body .= "--".$separator."--";
+
+			mail($emailaddress, $subject, $body, $headers);
 
 			$smarty->display('volunteer/thanks.tpl');
 		}
